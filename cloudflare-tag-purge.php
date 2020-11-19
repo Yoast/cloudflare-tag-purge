a84e2c9950be1016afa90162744e1b8dc7d70823
Peter van Wilderen <7113270+Petervw@users.noreply.github.com>
2020-11-19 13:58:50 +0100

    Downgraded PHP compatability to php 7.1

--- cloudflare-tag-purge.php
+++ cloudflare-tag-purge.php
@@ -4,13 +4,13 @@
  * Plugin URI: https://wordpress.org/plugins/cloudflare-yoast/
  * Description: Enables you to purge the cache by tags in Cloudflare (enterprise accounts only).
  * Author: Team Yoast
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author URI: https://wordpress.org/
  * Text Domain: cloudflare-tag-purge
  */
 
 
-define('cloudflare_tag_purge_version', '1.0.1');
+define('cloudflare_tag_purge_version', '1.0.2');
 
 add_action('init', 'yoast_cloudflare_admin_init');
 
@@ -33,7 +33,7 @@ function yoast_cloudflare_admin_init()
  * @param $update
  * @return mixed
  */
-function yoast_cloudflare_purge_action_save_post(int $post_id, $post, $update)
+function yoast_cloudflare_purge_action_save_post($post_id, $post, $update)
 {
     execute_yoast_tag_purge($post_id);
 }
@@ -44,7 +44,7 @@ function yoast_cloudflare_purge_action_save_post(int $post_id, $post, $update)
  * @param $update
  * @return mixed
  */
-function yoast_cloudflare_purge_action_edit_post(int $post_id, $post)
+function yoast_cloudflare_purge_action_edit_post($post_id, $post)
 {
     execute_yoast_tag_purge($post_id);
 }
@@ -52,7 +52,7 @@ function yoast_cloudflare_purge_action_edit_post(int $post_id, $post)
 /**
  * @param int $post_id
  */
-function execute_yoast_tag_purge(int $post_id)
+function execute_yoast_tag_purge($post_id)
 {
     $auth_key = read_yoast_environment_setting('CF_KEY', null);
 
@@ -62,7 +62,7 @@ function execute_yoast_tag_purge(int $post_id)
 
     $tags = get_yoast_cache_tags_by_post_id((int)$post_id);
 
-    if (\count($tags) === 0) {
+    if (count($tags) === 0) {
         return;
     }
 
@@ -73,11 +73,11 @@ function execute_yoast_tag_purge(int $post_id)
         return;
     }
 
-    $loops = \ceil(\count($tags) / 30); // a maximum of 30 tags per API call
+    $loops = ceil(count($tags) / 30); // a maximum of 30 tags per API call
     for ($i = 0; $i <= $loops; $i++) {
-        $api_tags = \array_splice($tags, ($i * 30), 30);
+        $api_tags = array_splice($tags, ($i * 30), 30);
 
-        \wp_remote_post(
+        wp_remote_post(
             'https://api.cloudflare.com/client/v4/zones/' . $zone_id . '/purge_cache',
             [
                 'method' => 'POST',
@@ -87,8 +87,8 @@ function execute_yoast_tag_purge(int $post_id)
                     'X-Auth-Key' => $auth_key,
                     'Content-Type' => 'application/json',
                 ],
-                'body' => \wp_json_encode([
-                    'tags' => \array_values($api_tags),
+                'body' => wp_json_encode([
+                    'tags' => array_values($api_tags),
                 ]),
             ]
         );
@@ -100,15 +100,15 @@ function execute_yoast_tag_purge(int $post_id)
  * @param int $post_id
  * @return array|string[]
  */
-function get_yoast_cache_tags_by_post_id(int $post_id)
+function get_yoast_cache_tags_by_post_id($post_id)
 {
     $tags = [get_yoast_cache_prefix() . 'postid-' . (int)$post_id];
     $post = get_post($post_id);
 
-    if ($post instanceof \WP_Post) {
+    if ($post instanceof WP_Post) {
         // add author page
         if (isset($post->post_author) && !empty($post->post_author)) {
-            $tags = \array_merge([get_yoast_cache_prefix() . 'author-' . $post->post_author], $tags);
+            $tags = array_merge([get_yoast_cache_prefix() . 'author-' . $post->post_author], $tags);
         }
 
         $post_type = get_post_type($post->ID);
@@ -143,7 +143,7 @@ function yoast_cache_prefix_body_class($classes)
         $prefixedClasses[$key] = get_yoast_cache_prefix() . $class;
     }
 
-    return \array_merge($classes, $prefixedClasses);
+    return array_merge($classes, $prefixedClasses);
 }
 
 /**
@@ -153,8 +153,8 @@ function yoast_cache_prefix_body_class($classes)
  */
 function read_yoast_environment_setting($key, $default)
 {
-    if (!empty(\getenv($key))) {
-        return \getenv($key);
+    if (!empty(getenv($key))) {
+        return getenv($key);
     }
 
     return $default;
@@ -170,8 +170,8 @@ function get_yoast_cache_prefix()
         return null;
     }
 
-    $prefix = \str_replace('.yoast.com', '', read_yoast_environment_setting('DOMAIN_CURRENT_SITE', 'staging-local.yoast.com'));
-    $prefix = \str_replace('.', '-', $prefix);
+    $prefix = str_replace('.yoast.com', '', read_yoast_environment_setting('DOMAIN_CURRENT_SITE', 'staging-local.yoast.com'));
+    $prefix = str_replace('.', '-', $prefix);
 
-    return trim(\strtolower($prefix)) . '-';
+    return trim(strtolower($prefix)) . '-';
 }
